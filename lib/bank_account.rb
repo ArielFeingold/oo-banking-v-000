6692f26c21ead972173ae7cb2dcc94c991216120
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 19:56:59 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -20,7 +20,8 @@ attr_reader :name
 
   def valid?
     # binding.pry
-    (@status = "open") && @balance > 0.to_i ? true : false
+    @status = "open" && @balance > 0.to_i ? true : false
+    binding.pry
   end
 
 
