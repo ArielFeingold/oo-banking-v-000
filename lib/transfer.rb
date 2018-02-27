024e473648aa65816a75a879c57dfeb276b997ce
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 20:12:07 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,5 +18,6 @@ class Transfer
     def execute_transaction
         sender.balance = sender.balance - @amount
         receiver.balance = receiver.balance + @amount
+        @status = "complete"
     end
 end
