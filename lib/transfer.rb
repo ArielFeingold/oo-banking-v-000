9c59809e8d5bbbfc2b0757a8f9ccf09f1621dd14
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 21:20:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
 
     def execute_transaction
       # binding.pry
-      if self.valid? == true
+      if self.valid? == true && @status != "complete"
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
