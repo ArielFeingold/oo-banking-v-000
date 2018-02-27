8697a40a749edbd2c7145cccad5ec87dff06c125
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 20:24:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
         sender.balance = sender.balance - @amount
         receiver.balance = receiver.balance + @amount
         @status = "complete"
+      else return "Transaction rejected. Please check your account balance."
       end
     end
 end
