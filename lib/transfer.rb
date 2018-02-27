e9a3b7955b419325bd902e733138bd9a508d83d3
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 21:12:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,12 +17,13 @@ class Transfer
 
     def execute_transaction
       # binding.pry
-      if !self.valid?
-        "Transaction rejected. Please check your account balance."
-      else
+      if self.valid?
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
+      else
+        self.valid? = false
+          "Transaction rejected. Please check your account balance."
       end
     end
 end
