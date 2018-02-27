93787f124ffc8aa8a249bb0bf1ae0afadbe11005
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 20:18:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
         sender.balance = sender.balance - @amount
         receiver.balance = receiver.balance + @amount
         @status = "complete"
-        self.delete
+  
 
 
     end
